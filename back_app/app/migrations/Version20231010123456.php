00b3c29b10f65e6aaffa18d7676ed93e24fb7ca5
Rearius <alexey@introvert.bz>
2025-02-01 20:44:30 +0300

    added migration for user entity

---
+++ back_app/app/migrations/Version20231010123456.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231010123456 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create User entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, tab_num INT NOT NULL, gr_kod INT NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE "user"');
+    }
+}
