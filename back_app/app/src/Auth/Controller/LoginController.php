d696331c61efd279e4f0d0bff52a6c36e1075af8
Rearius <alexey@introvert.bz>
2025-02-09 20:30:31 +0300

    Changed folders structure on domain type

--- back_app/app/src/Auth/Controller/LoginController.php
+++ back_app/app/src/Auth/Controller/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Auth;
+namespace App\Auth\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
--- back_app/app/src/User/Controller/UserController.php
+++ back_app/app/src/User/Controller/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\User\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
